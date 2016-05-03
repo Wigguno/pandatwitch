b4c368d7da0e2b63907dc4280f6b444442f17734
hherman1 <hherman1@macalester.edu>
2016-05-03 14:14:48 -0500

    Merged Wigguno's changes + added several features

--- pandatwitch.user.js
+++ pandatwitch.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Panda Twitch Chat
-// @version      0.9
+// @version      1.0
 // @description  Modifies Panda.tv to be more like twitch theater mode
-// @author       wigguno
+// @author       wigguno, hherman1
 // @match        http://www.panda.tv/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -17,26 +17,22 @@ var url = window.location.href.split("/");
 var id = url[url.length-1];
 
 var localstorage_settings_id = "panda-settings";
-var default_associations = {"3332":"arteezy"};
-
-if($.isEmptyObject({})) {
-    setSettings(default_associations);
-}
+var default_associations = {"3331":"eternalenvyy", "3332":"arteezy", "3333":"universedota", "3334":"puppey", "3335":"pieliedie"};
 
 var twitch_lut = getSettings();
-if (!(id in twitch_lut))
-    return;
 
+
+if (!(id in twitch_lut)) id = -1; // default to whatever you set to -1
 var twitch_username = twitch_lut[id];
 waitForKeyElements (".room-rank-container", PTC);
 
 // ------------------------------------------------------------------------------------------------------------------------
 // Settings
-var b_insert_twitch = true;
-var b_move_panda = true;
-var b_rearrange_footer = true;
-var b_remove_detail = true;
-var b_darktheme = true;
+var b_insert_twitch    = GM_getValue('insert_twitch', true);
+var b_move_panda       = GM_getValue('move_panda', false);
+var b_rearrange_footer = GM_getValue('rearrange_footer', true);
+var b_remove_detail    = GM_getValue('remove_detail', true);
+var b_darktheme        = GM_getValue('darktheme', true);
 var b_settings = true;
 
 // ------------------------------------------------------------------------------------------------------------------------
@@ -57,50 +53,110 @@ function PTC() {
     // Insert twitch chat
     if (b_insert_twitch === true)
         document.getElementsByClassName("room-chat-box")[0].innerHTML="<iframe src='https://www.twitch.tv/" + twitch_username + "/chat' width='100%' height='100%' />";
-    
+
     if (b_settings === true) {
         add_settings();
     }
 }
 
 // ------------------------------------------------------------------------------------------------------------------------
-// Settings functions        
+// Settings functions
 
 function add_settings()
 {
-    var parent = $(".room-head-info");
-    var settings_menu = $("<div class=panda-twitch-settings>" +
-                             "<div class=content>"+
-                                   "<div class=panda-twitch-dropdown>" +
-                                            "<ul id=panda-twitch-chat-conversions>" +
-                                            "</ul>" +
-                                            "<hr></hr>"+
-                                            "<div id=association-adder>"+
-                                                 "<table>"+
-                                                      "<tr> <td>PandaTV ID</td> <td><input type=text name=panda-id></input></td></tr>"+
-                                                      "<tr> <td>Twitch Account Name</td> <td><input type=text name=twitch-name></input></td></tr>"+
-                                                      "<tr> <td><button id=add-relationship> Add/Update </button></td></tr>" +
-                                                 "</table>"+
-                                            "</div>"+
-                                   "</div>" +
-                             "</div>"+
-                               "<div class=dropdown-opener>" +
-                                    "PandaTwitch Settings" +
-                               "</div>" +
-                               
-                          "</div>");
+    var parent = (b_rearrange_footer) ? $(".room-head-info") : $(".room-foot-box");
+    var settings_menu = $(	"<div class=panda-twitch-settings>" +
+					"<div class=content>"+
+						"<div class=panda-twitch-dropdown>" +
+							"<ul id=panda-twitch-chat-conversions>" +
+							"</ul>" +
+							"<hr></hr>"+
+							"<div class='settings-table-container' id=association-adder>"+
+								"<table>"+
+									"<tr> <td>PandaTV ID</td> <td><input type=text name=panda-id></input></td></tr>"+
+									"<tr> <td>Twitch Account Name</td> <td><input type=text name=twitch-name></input></td></tr>"+
+									"<tr> <td><button id=add-relationship> Add/Update </button></td> <td> <button id=reset-relationships> Reset to Defaults </button></tr>" +
+								"</table>"+
+							"</div>"+
+							"<hr></hr>"+
+							"<div class='settings-table-container' id=settings-table>"+
+								"<center><table>"+
+									"<tr> <td>Insert Twitch Chat</td> <td><input type=checkbox id=ptset-twitch></input></td></tr>"+
+									"<tr> <td>Move Panda Chat</td>    <td><input type=checkbox id=ptset-panda></input></td></tr>"+
+									"<tr> <td>Rearrange Footer</td>   <td><input type=checkbox id=ptset-footer></input></td></tr>"+
+									"<tr> <td>Remove Detail</td>      <td><input type=checkbox id=ptset-detail></input></td></tr>"+
+									"<tr> <td>Dark Theme</td>         <td><input type=checkbox id=ptset-dark></input></td></tr>"+
+								"</table></center>"+
+							"</div>" +
+						"</div>" +
+					"</div>"+
+					"<div class=dropdown-opener>" +
+						"PandaTwitch Settings" +
+					"</div>" +
+				"</div>");
     $("#panda-twitch-chat-conversions",settings_menu).append(generateAssociationsDOM()); // add existing settings
     $(".panda-twitch-dropdown",settings_menu).hide(); // hide by default
     $(".dropdown-opener",settings_menu).click(function(){
         $(".panda-twitch-dropdown",settings_menu).toggle(); // open on click
     });
+    $("#association-adder input",settings_menu).keyup(function(event){
+	    if(event.keyCode == 13){
+	        $("#association-adder #add-relationship").click();
+	    }
+    });
     $("#association-adder #add-relationship",settings_menu).click(function() {
+    	var old_height = $(".panda-twitch-dropdown",settings_menu).prop("scrollHeight");
+    	var scroll =  $(".panda-twitch-dropdown",settings_menu).scrollTop();
         var panda_tv_id = $("#association-adder [name=panda-id]",settings_menu).val();
         var twitch_name = $("#association-adder [name=twitch-name]",settings_menu).val();
         add_association(panda_tv_id,twitch_name);
         $("#panda-twitch-chat-conversions",settings_menu).empty(); // refresh all old assocations
         $("#panda-twitch-chat-conversions",settings_menu).append(generateAssociationsDOM());// show new association
+        $(".panda-twitch-dropdown",settings_menu).scrollTop(scroll + $(".panda-twitch-dropdown",settings_menu).prop("scrollHeight") - old_height);
         $("#association-adder input",settings_menu).val(''); //clear inputs
+    });
+    $("#association-adder #reset-relationships",settings_menu).click(function() {
+    	var old_height = $(".panda-twitch-dropdown",settings_menu).prop("scrollHeight");
+    	var scroll =  $(".panda-twitch-dropdown",settings_menu).scrollTop();
+        setSettings(default_associations);
+        $("#panda-twitch-chat-conversions",settings_menu).empty(); // refresh all old assocations
+        $("#panda-twitch-chat-conversions",settings_menu).append(generateAssociationsDOM());// show new association
+        $(".panda-twitch-dropdown",settings_menu).scrollTop(scroll + $(".panda-twitch-dropdown",settings_menu).prop("scrollHeight") - old_height);
+        $("#association-adder input",settings_menu).val(''); //clear inputs
+    });
+    $("#settings-table #ptset-twitch", settings_menu).prop('checked', b_insert_twitch);
+    $("#settings-table #ptset-twitch", settings_menu).click(function() {
+        b_insert_twitch = $("#settings-table #ptset-twitch",settings_menu).prop('checked');
+        GM_setValue('insert_twitch', b_insert_twitch);
+        update_css_settings();
+    });
+
+    $("#settings-table #ptset-panda", settings_menu).prop('checked', b_move_panda);
+    $("#settings-table #ptset-panda", settings_menu).click(function() {
+        b_move_panda = $("#settings-table #ptset-panda",settings_menu).prop('checked');
+        GM_setValue('move_panda', b_move_panda);
+        update_css_settings();
+    });
+
+    $("#settings-table #ptset-footer", settings_menu).prop('checked', b_rearrange_footer);
+    $("#settings-table #ptset-footer", settings_menu).click(function() {
+        b_rearrange_footer = $("#settings-table #ptset-footer",settings_menu).prop('checked');
+        GM_setValue('rearrange_footer', b_rearrange_footer);
+        update_css_settings();
+    });
+
+    $("#settings-table #ptset-detail", settings_menu).prop('checked', b_remove_detail);
+    $("#settings-table #ptset-detail", settings_menu).click(function() {
+        b_remove_detail = $("#settings-table #ptset-detail",settings_menu).prop('checked');
+        GM_setValue('remove_detail', b_remove_detail);
+        update_css_settings();
+    });
+
+    $("#settings-table #ptset-dark", settings_menu).prop('checked', b_darktheme);
+    $("#settings-table #ptset-dark", settings_menu).click(function() {
+        b_darktheme = $("#settings-table #ptset-dark",settings_menu).prop('checked');
+        GM_setValue('darktheme', b_darktheme);
+        update_css_settings();
     });
     parent.append(settings_menu);
 }
@@ -134,7 +190,7 @@ function delete_association(panda_tv_id) {
 }
 function getSettings() {
     try {
-        return JSON.parse(GM_getValue(localstorage_settings_id));
+        return JSON.parse(GM_getValue(localstorage_settings_id, {}));
     } catch(e) {
         return {};
     }
@@ -235,21 +291,21 @@ var css_rearranged_footer = ""+
 
 
 var css_settings = ""+
-".panda-twitch-settings                                         {     position: relative; float:right; margin-right:10px; }"+
+".panda-twitch-settings                                         { position: relative; float:right; margin-right:10px; }"+
 ".panda-twitch-settings .dropdown-opener                        { background-color: white; padding: 10px;}"+
 ".panda-twitch-settings .dropdown-opener:hover                  { color:darkgray; cursor:pointer;}"+
 ".panda-twitch-settings .content                                { position:relative }"+
-".panda-twitch-settings .panda-twitch-dropdown                  { position:absolute;right:0;bottom:0; background-color: white; display: inline-block; padding: 10px;}"+
+".panda-twitch-settings .panda-twitch-dropdown                  { position:absolute;right:0;bottom:0; background-color: white; display: inline-block; padding: 10px; max-height:500px; overflow-y:scroll;}"+
 ".panda-twitch-settings .panda-twitch-dropdown li               { margin:10px;}"+
 ".panda-twitch-settings .panda-twitch-dropdown li button        { float:right;}"+
-".panda-twitch-settings #association-adder td                   { padding:5px;}"+
-".panda-twitch-settings #association-adder input                {}"+
-".panda-twitch-settings #association-adder button               { margin:10px;}";
+".panda-twitch-settings .settings-table-container td            { padding:5px;}"+
+".panda-twitch-settings .settings-table-container input         {}"+
+".panda-twitch-settings .settings-table-container button        { margin:10px;}";
 
-var v_css_move_panda;
-var v_css_rearranged_footer;
-var v_css_darktheme;
-var v_css_settings;
+var v_css_move_panda = {val:-1};
+var v_css_rearranged_footer = {val:-1};
+var v_css_darktheme = {val:-1};
+var v_css_settings = {val:-1};
 
 if (b_move_panda === true) {
     //GM_log('[css] moving panda chat');
@@ -271,13 +327,41 @@ if (b_settings === true) {
     enable_css(v_css_settings, css_settings);
 }
 
+update_css_settings();
+function update_css_settings()
+{
+    if (b_move_panda === true) {
+        enable_css(v_css_move_panda, css_move_panda);
+    }
+    else {
+        disable_css(v_css_move_panda);
+    }
+
+    if (b_rearrange_footer === true) {
+        enable_css(v_css_rearranged_footer, css_rearranged_footer);
+    }
+    else {
+        disable_css(v_css_rearranged_footer);
+    }
+
+    if (b_darktheme === true) {
+        enable_css(v_css_darktheme, css_darktheme);
+    }
+    else {
+        disable_css(v_css_darktheme);
+    }
+}
+
 function enable_css(v_css, css)
 {
-    if (v_css === undefined)
-        v_css = addStyle(css);
+    if (v_css.val == -1)
+        v_css.val = addStyle(css);
 }
 function disable_css(v_css)
 {
-    if (v_css !== null)
-        document.getElementsByTagName('head')[0].removeChild(v_css);
+    if (v_css.val != -1)
+    {
+        document.getElementsByTagName('head')[0].removeChild(v_css.val);
+        v_css.val = -1;
+    }
 }
