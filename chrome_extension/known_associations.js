ec76650206c342f63325f4550fa8ccba91c15a22
Richard <wigguno@gmail.com>
2016-05-05 18:40:55 +1200

    remove chrome extension for now

--- chrome_extension/known_associations.js
+++
@@ -1,8 +0,0 @@
-var known_associations = {
-  "-1":"arteezy", //default channel
-  "3331":"eternalenvyy",
-  "3332":"arteezy",
-  "3333":"universedota", 
-  "3334":"puppey", 
-  "3335":"pieliedie",
-}
